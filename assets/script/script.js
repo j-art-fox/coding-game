e9929c3091a80fbd9e8398031f376e21aea8870d
jordanrings <jordanringsakabe@gmail.com>
2022-07-08 12:40:16 -0600

    created 100second timer

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,40 @@
-var startQuizCont = document.querySelector(".start-quiz");
+var startQuizBtn = document.querySelector("#start-button");
 var gameCardCont = document.querySelector(".game-card");
+var timeEl = document.querySelector(".timer");
+var timeLeft = 10;
 var possAnsEl1 = document.getElementById("possible-answer1");
 var possAnsEl2 = document.getElementById("possible-answer2");
 var possAnsEl3 = document.getElementById("possible-answer3");
 var possAnsEl4 = document.getElementById("possible-answer4");
+var startWindowEl = document.querySelector(".start-window");
 
 
-startQuizCont.addEventListener("click",startQuiz);
+function startQuiz() {
+    startWindowEl.setAttribute("style", "display:none;");
+    gameCardCont.setAttribute("style", "display:flex;");
+    setTime();
+    console.log("bacon");
+};
 
-function startQuiz (){}
-// display gamecard
-// start timer
+startQuizBtn.addEventListener("click", startQuiz);
+
+function setTime() {
+    var timerInterval = setInterval(function () {
+        timeLeft--;
+        timeEl.textContent = timeLeft + " seconds remaining";
+
+        if (timeLeft === 0) {
+            clearInterval(timerInterval);
+            sendMessage();
+            console.log("time has hit 0.");
+        }
+    }, 1000)
+}
+
+function sendMessage(){
+    timeEl.textContent = "Game Over";
+}
 
 
-function showNextQuestion(){}
+// function showNextQuestion() { }
 
