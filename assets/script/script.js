cfddefc07feb0b4fa9ab7ea57bdd9798490eea6f
jordanrings <jordanringsakabe@gmail.com>
2022-07-08 11:37:22 -0600

    added basic javascript and laying down process for continuing with project

---
+++ assets/script/script.js
@@ -0,0 +1,17 @@
+var startQuizCont = document.querySelector(".start-quiz");
+var gameCardCont = document.querySelector(".game-card");
+var possAnsEl1 = document.getElementById("possible-answer1");
+var possAnsEl2 = document.getElementById("possible-answer2");
+var possAnsEl3 = document.getElementById("possible-answer3");
+var possAnsEl4 = document.getElementById("possible-answer4");
+
+
+startQuizCont.addEventListener("click",startQuiz);
+
+function startQuiz (){}
+// display gamecard
+// start timer
+
+
+function showNextQuestion(){}
+
